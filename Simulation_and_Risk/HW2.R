fb280e5a581d74f133ad2273c33a099f7d2acab4
ccnobbli <ccnobbli@ncsu.edu>
2018-11-12 20:59:47 -0500

    cleaned up jerry's code

--- Simulation_and_Risk/HW2.R
+++ Simulation_and_Risk/HW2.R
@@ -6,48 +6,28 @@
 ####   Chris      ########
 #### 11/12/2018   ########
 
-
 library(ks)
-library(dplyr)
-library(EnvStats)
 library(triangle)
-library(readxl)
 library(rlist)
-  
-# Needed Libraries for Analysis #
-
 library(graphics)
-
-# install.packages("xlsx")
-
-# library("xlsx")
-
-# install.packages("readxl") # CRAN version
-
 library(readxl)
-
 library(dplyr)
-
-#install.packages("EnvStats") # CRAN version
-
 library(EnvStats)
-
 library(ggplot2)
 
 #set up working directory
-
-setwd("C:/Users/Jerry/Documents/MSA18/Simulation_Risk_Analysis/HW/")
-
-# simulation times 
-n_sims <- 10000
-n = 10000
+#setwd("C:/Users/Jerry/Documents/MSA18/Simulation_Risk_Analysis/HW/")
+setwd("C:/Users/Christopher/Documents/IAA/Fall 3/simulation")
+  
+  
+# number of single well simulations
+n_sims <- 100000
+n = 100000
 
 #import data from the XLSX 
-
 drill = read_excel("Analysis_Data.xlsx", sheet = 2, skip = 2)
 
 #shortens data set to relivent years 
-
 drill1 <- drill %>% filter(Date >= "1991-06-01") %>% filter(Date <= "2006-07-01")
 
 #change character to a numeric 
@@ -66,25 +46,8 @@ drill1$Average.Cost = ((drill1$`U.S. Nominal Cost per Crude Oil Well Drilled (Th
                           
                           drill1$`U.S. Nominal Cost per Dry Well Drilled (Thousand Dollars per Well)`)/3)
 
-#histogram for average cost and average return
-
-hist(c(drill1$Return.Crude.Oil, drill1$Return.Natural.Gas, drill1$Return.Dry.Well), 
-     
-     main = "Arithmetic Change in Cost Distribution",xlab="Arithmetic Return", col = "#6699FF", breaks = 24)
-
-# Basic histogram
 
 x = as.data.frame(c(drill1$Return.Crude.Oil, drill1$Return.Natural.Gas, drill1$Return.Dry.Well))
-
-ggplot(x, aes(x=c(drill1$Return.Crude.Oil, drill1$Return.Natural.Gas, drill1$Return.Dry.Well))) +
-  
-  geom_histogram(fill="#6699FF", color="black", binwidth=.075)+
-  
-  #  geom_histogram(fill="#6699FF", color="black")+
-  
-  labs(title="Arithmetic Change in Cost Distribution",x="Arithmetic Return", y = "Frequency")+
-  
-  theme_minimal() + theme(title = element_text(size = 18), axis.title.x = element_text(size =14), axis.title.y = element_text(size =14))
 
 #-------------------------Kernel Density---------------------------------#
 
@@ -98,56 +61,18 @@ Density.R
 
 Est.R <- rkde(fhat=kde(c(drill1$Return.Crude.Oil,drill1$Return.Natural.Gas,drill1$Return.Dry.Well), h=0.07935), n=10000)
 
-hist(Est.R, breaks=50, main='Estimated 2006 to 2012 Return Value Distribution', xlab='Return')
-
 Est.Hist = as.data.frame(Est.R)
 
-ggplot(Est.Hist, aes(x=Est.R)) +
-  
-  #geom_histogram(fill="#6699FF", color="black", binwidth=.075)+
-  
-  geom_histogram(fill="#6699FF", color="black")+
-  
-  labs(title="Estimated 2006 to 2012 Return Value Distribution",x="Return", y = "Frequency")+
-  
-  theme_minimal() + theme(title = element_text(size = 18), axis.title.x = element_text(size =14), axis.title.y = element_text(size =14))
-
-#qq-plot -- not sure which one to look at
-
-qqnorm(c(drill1$Return.Crude.Oil,drill1$Return.Natural.Gas,drill1$Return.Dry.Well), col = "#6699FF", main="QQ Plot Historical Data")
-
-qqline(c(drill1$Return.Crude.Oil,drill1$Return.Natural.Gas,drill1$Return.Dry.Well), col = "2")
-
-#Shapiro-Wilk normality test p-value = 0.8041
-
-shapiro.test(c(drill1$Return.Crude.Oil,drill1$Return.Natural.Gas,drill1$Return.Dry.Well))
-
-summary(c(drill1$Return.Crude.Oil,drill1$Return.Natural.Gas,drill1$Return.Dry.Well))
-
-kurtosis(c(drill1$Return.Crude.Oil,drill1$Return.Natural.Gas,drill1$Return.Dry.Well))
 
 #-------------------------Simulation---------------------------------#
 
 ##############Simulation1: Kernel Density ####################################### 
 
-#should I use triangular dist. for r2 and r3?
-
-#seems wrong, cannot assume mean is mode
-
-#but cannot use normal dist. as well, b/c don't know std
-
-#need discussion
-
 cost_k <- rep(0,n)
-
-class(cost_k)
 
 for (i in 1:n){
   
-  
-  
   r1 <- rkde(fhat=kde(c(drill1$Return.Crude.Oil,drill1$Return.Natural.Gas,drill1$Return.Dry.Well),
-                      
                       h=0.07935), n=6)
   
   r2 <- rtri(n=3, mode = -0.0917, min = -0.22, max = -0.07)
@@ -165,36 +90,19 @@ for (i in 1:n){
   for (j in 1:13){
     
     P <- P*(1+r[j]) # give the cost prediction for 2019 
-    
   }
-  
   cost_k[i] <- P # outputs cost prediciton for 2019 to vector "cost_k"
-  
 }
-
-summary(cost_k) #summary statistics for cost_k 
 
 #Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
 
 #367.4  2577.8  3476.5  3769.7  4634.9 27030.9 
 
-quantile(cost_k, probs = c(0.05, 0.95))
-
 #    5%      95% 
-
 #  1643.848 6893.007
 
-quantile(cost_k, probs = c(0.01, 0.99))
-
 #    1%      99% 
-
 #  1187.543 9006.787 
-
-hist(cost_k, breaks=100, main='2019 Cost Prediction Distribution KD', xlab='Cost', col = "#6699FF")
-
-abline(v = drill1$Average.Cost[16], col="red", lwd=2)
-
-mtext("2006 Cost", at=drill1$Average.Cost[16], col="red")
 
 #-------------------------Simulation---------------------------------#
 
@@ -202,7 +110,6 @@ mtext("2006 Cost", at=drill1$Average.Cost[16], col="red")
 
 cost_n <- rep(0,n)
 
-class(cost_n)
 
 for (i in 1:n){
   r1 <- rnorm(n=6, mean = mean(c(drill1$Return.Crude.Oil,drill1$Return.Natural.Gas,drill1$Return.Dry.Well)), 
@@ -229,45 +136,20 @@ for (i in 1:n){
   
 }
 
-summary(cost_n) #summary statistics for cost_n 
-
-#Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-
-#292.2  2670.8  3528.6  3770.5  4600.2 16280.9 
-
-quantile(cost_n, probs = c(0.05, 0.95))
-
-#   5%      95% 
-
-#  1748.740 6618.248 
-
-quantile(cost_n, probs = c(0.01, 0.99))
-
-#   1%      99% 
-
-#  1270.605 8434.950 
-
-hist(cost_n, breaks=100, main='2019 Cost Prediction Distribution N', xlab='Cost', col = "#6699FF")
-
-abline(v = drill1$Average.Cost[16], col="red", lwd=2)
-
-mtext("2006 Cost", at=drill1$Average.Cost[16], col="red")
-
-
 # Year "0" Costs ----------------------------------------------------------
 
 #  leased acres per well - Normally Distributed
 #  Each acre costs us $960 (this will be a recurring annual cost if we find a "WET" well)
-lease_costs <- rnorm(n = 50, mean = 600, sd = 50) * 960
+lease_costs <- rnorm(n = n_sims, mean = 600, sd = 50) * 960
 
 # Number of seismic sections per well, Normally Distributed
 # Each section (per well) costs us $43,000
-seismic_costs <- rnorm(n = 50, mean = 3, sd = 0.35) * 43000
+seismic_costs <- rnorm(n = n_sims, mean = 3, sd = 0.35) * 43000
 
 # These costs will only be incurred IF the well is "WET"
 # So, "DRY" wells will not incur this cost
 # Normally distributed
-completion_costs <- rnorm(n=50, mean = 390000, sd=50000)
+completion_costs <- rnorm(n=n_sims, mean = 390000, sd=50000)
 
 # These costs always occur year 0, and recur only if the well is "WET"
 # So, "DRY" wells only have this cost for year 0
@@ -288,10 +170,8 @@ overhead_costs <- rtriangle(n=n_sims, a = 172000, b = 279500, c=215000)
 n_wells <- n_sims # the number of wells we are considering
 n_years <- length(seq(2019,2050)) # the number of years
 
-Value.r <- rep(0, n_wells)
 R <- matrix(data=cbind(1, 0.64, 0.64, 1), nrow=2)
 U <- t(chol(R))
-
 
 standardize <- function(x){
 x.std = (x - mean(x))/sd(x)
@@ -303,14 +183,8 @@ x.old = (x.std * sd(x)) + mean(x)
 return(x.old)
 }
 
-# Placeholder for the final correlated values of initial produciton and rate of decline
-final.ip <-  rep(0, n_wells)
-final.rod <- rep(0, n_wells)
-
 # Create correlated values of initial production costs and rates of decline
-
 # Underlying lognormal distribution should have mean ~ 6 and sd ~0.28
-# TODO fix sdlog - does not produce dist with sd of 0.28
 initial_production <- rlnorm(n = n_wells, meanlog = 6, sdlog = 0.28)
 rate_of_decline <- runif(n = n_wells, min = 15, max = 32 )
 
@@ -325,9 +199,6 @@ final.rod <- destandardize(SB.r[,1], rate_of_decline) / 100
 
 # Any negative rates corresond to a "dry" well
 final.ip <- ifelse(final.ip < 0, 0, final.ip)
-
-View(final.ip)
-
 
 # Check the correlation, if desired - my initial tests showed it was approx. 0.64
 # cor(final.ip, final.rod)
@@ -349,50 +220,66 @@ bopd_year_beg[,1] <- final.ip
 
 # This could maybe be optimized using matrix math, but for loops will have to do for now
 for (year in seq(1:n_years)) {
-for (well_num in seq(1:n_wells)) {
-  if (year != 1) {
-    bopd_year_beg[well_num, year] = bopd_year_end[well_num, year-1]
+  for (well_num in seq(1:n_wells)) {
+    if (year != 1) {
+      bopd_year_beg[well_num, year] = bopd_year_end[well_num, year-1]
+    }
+    bopd_year_end[well_num, year] = (1 - final.rod[well_num])*bopd_year_beg[well_num, year]
+    yearly_volume[well_num, year] = 365 * (bopd_year_beg[well_num, year] + bopd_year_end[well_num, year])/2
   }
-  bopd_year_end[well_num, year] = (1 - final.rod[well_num])*bopd_year_beg[well_num, year]
-  yearly_volume[well_num, year] = 365 * (bopd_year_beg[well_num, year] + bopd_year_end[well_num, year])/2
-}
 }
 
-View(yearly_volume)
-plot(yearly_volume[6,])
 
+# Calculate Net Present Revenue (NPR) -----------------------------------------------------------------
 
-# revenue -----------------------------------------------------------------
-
-oil_pred <- read_excel("C:/Users/Jerry/Documents/MSA18/Simulation_Risk_Analysis/HW/Analysis_Data.xlsx")
-View(oil_pred)
-colnames(oil_pred) <- oil_pred[2,]
+# Jerry's access
+#oil_pred <- read_excel("C:/Users/Jerry/Documents/MSA18/Simulation_Risk_Analysis/HW/Analysis_Data.xlsx")
 oil_pred <- oil_pred[c(3:nrow(oil_pred)),]
 oil_pred <- data.frame(oil_pred)
 oil_pred$High.Oil.Price <- as.numeric(oil_pred$High.Oil.Price)
 oil_pred$Low.Oil.Price <- as.numeric(oil_pred$Low.Oil.Price)
 oil_pred$AEO2018.Reference <- as.numeric(oil_pred$AEO2018.Reference)
 
+# Chris' access 
+file_path <- "C:/Users/Christopher/Documents/IAA/Fall 3/F3-Blueteam12/Simulation_and_Risk/price_projections.csv"
+column_names = c("year", "high_price", "low_price", "aeo_ref")
+oil_pred <- read_csv(file = file_path,
+                     skip = 3,
+                     col_names = column_names)
+colnames(oil_pred) <- column_names
+oil_pred$high_price <- as.numeric(oil_pred$high_price)
+oil_pred$low_price <- as.numeric(oil_pred$low_price)
+oil_pred$aeo_ref <- as.numeric(oil_pred$aeo_ref)
+
+
+severance_tax <- (1 - 0.046)
 rev <- c()
-for (i in 1:n_years) {
-    oil_price <- rtriangle(n = n_sims, a = oil_pred[i, 3], b = oil_pred[i, 2], c = oil_pred[i, 4])
-    production <- yearly_volume[, i]
-    nri <- rnorm(n = n_sims, mean = 0.75, sd = 0.02)
-    rev_adj <- oil_price*production*nri*0.954
-    rev <- cbind(rev, rev_adj)
-}
-
-colnames(rev) <- seq(2019, 2050)
-
-# operating expense -------------------------------------------------------
-
 operating_cost <- c()
-for (i in 1:n_years) {
-  cost <- rnorm(n = n_sims, mean = 2.25, sd = 0.3)
-  production <- yearly_volume[, i]
-  total_operating_cost <- cost*production
-  operating_cost <- cbind(operating_cost, total_operating_cost)
+
+
+for (year in 1:n_years) {
+  
+    # Draw the oil price from a random triangle distribution
+    oil_price <- rtriangle(n = n_sims, a = oil_pred$low_price[year],
+                           b = oil_pred$high_price[year],
+                           c = oil_pred$aeo_ref[year])
+    
+    #Grab that year's total volume produced
+    production <- yearly_volume[, year]
+    
+    # Draw a net revenue interest (nri) from a normal distribution
+    nri <- rnorm(n = n_sims, mean = 0.75, sd = 0.02)
+    
+    # Calculate revenue for the year
+    rev <- cbind(rev,
+                 oil_price*production*nri*severance_tax)
+    
+    # Now calculate total operating costs
+    cost <- rnorm(n = n_sims, mean = 2.25, sd = 0.3)
+    total_operating_cost <- cost*production
+    operating_cost <- cbind(operating_cost, total_operating_cost)
 }
+
 
 # NPV ---------------------------------------------------------------------
 
@@ -401,4 +288,5 @@ for (i in 1:n_years) {
   sum <- sum + (rev[, i] - operating_cost[, i] - lease_costs[i] - overhead_costs[i]) / (1.1^i)
 }
 NPV <- -(cost_n + seismic_costs + completion_costs) + sum
-NPV
+
+hist(NPV)
