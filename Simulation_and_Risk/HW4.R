dc3bf4f5a8b7f4ba1ab6302bd42fc639774b5824
Jerryliao <chejuiliao@users.noreply.github.com>
2018-11-23 17:16:45 -0500

    fix the drilling cost unit and the price prediction

--- Simulation_and_Risk/HW4.R
+++ Simulation_and_Risk/HW4.R
@@ -15,8 +15,9 @@ library(readr)
 setwd("C:/Users/Jerry/Documents/MSA18/Simulation_Risk_Analysis/HW/")
 
 # number of single well simulations
-n_sims <- 10000
-n_years <- length(seq(2019,2050)) # the number of years
+n_sims <- 1000
+# the number of years
+n_years <- 15
 set.seed(8888)
 
 # oil price 
@@ -86,6 +87,7 @@ for (i in 1:n_sims) {
     }
     drilling_cost[i] <- P # outputs cost prediciton for 2019 to vector "cost_k"
   }
+  drilling_cost <- drilling_cost * 1000
   
   # Year "0" Costs ----------------------------------------------------------
   #  leased acres per well - Normally Distributed
@@ -99,21 +101,14 @@ for (i in 1:n_sims) {
   # These costs will only be incurred IF the well is "WET"
   # So, "DRY" wells will not incur this cost
   # Normally distributed
-  if (n_wet != 0){
-    completion_costs <- rnorm(n = n_wet, mean = 390000, sd=50000)
-  } else {
-    completion_costs <- 0
-  }
+  completion_costs <- rnorm(n = n_wet, mean = 390000, sd=50000)
+
   
   # These costs always occur year 0, and recur only if the well is "WET"
   # So, "DRY" wells only have this cost for year 0
   # It remains constant throughout a well's lifetime
   # Triangular distribution
-  if (n_wet != 0) {
-    overhead_costs_wet <- rtriangle(n=n_wet, a = 172000, b = 279500, c=215000)
-  } else {
-    overhead_costs_wet <- 0
-  }
+  overhead_costs_wet <- rtriangle(n=n_wet, a = 172000, b = 279500, c=215000)
   
   if (n_dry != 0){
     overhead_costs_dry <- rtriangle(n=n_dry, a = 172000, b = 279500, c=215000)
@@ -126,104 +121,99 @@ for (i in 1:n_sims) {
   # 1. Initial production  (lognormally distributed)
   # 2. Decline Rate (uniform distribution, b/w 15% and 32%)
   # These values are correlated (0.64), so we will use a correlation matrix to bend the random initial
+  R <- matrix(data=cbind(1, 0.64, 0.64, 1), nrow=2)
+  U <- t(chol(R))
   
-  if (n_wet != 0) {
-    R <- matrix(data=cbind(1, 0.64, 0.64, 1), nrow=2)
-    U <- t(chol(R))
-    
-    standardize <- function(x){
-      x.std = (x - mean(x))/sd(x)
-      return(x.std)
-    }
-    
-    destandardize <- function(x.std, x){
-      x.old = (x.std * sd(x)) + mean(x)
-      return(x.old)
-    }
-    
-    # Create correlated values of initial production costs and rates of decline
-    # Underlying lognormal distribution should have mean ~ 6 and sd ~0.28
-    initial_production <- rlnorm(n = n_wet, meanlog = 6, sdlog = 0.28)
-    rate_of_decline <- runif(n = n_wet, min = 15, max = 32)
-    
-    # Standardize the vectors and "bend" them to correlate
-    Both.r <- cbind(standardize(rate_of_decline), standardize(initial_production))
-    SB.r <- U %*% t(Both.r)
-    SB.r <- t(SB.r)
-    
-    # Unstandardize the vectors
-    final.ip  <- destandardize(SB.r[,2], initial_production)
-    final.rod <- destandardize(SB.r[,1], rate_of_decline) / 100
-    
-    # Any negative rates corresond to a "dry" well
-    final.ip <- ifelse(final.ip < 0, 0, final.ip)
-    
-    # Check the correlation, if desired - my initial tests showed it was approx. 0.64
-    # cor(final.ip, final.rod)
-    
-    # Now calculate estimated production rates for 30 years using the calculated
-    # initial production and yearly decline rate
-    
-    # These matrices represent the production of oil in barrels per day
-    # Each row is one well
-    # Each column is the year (col 1 == year 1)
-    bopd_year_beg <- matrix(0L, nrow=n_wet, ncol = n_years)
-    bopd_year_end <- matrix(0L, nrow=n_wet, ncol = n_years)
-    
-    # This matrix represents the total volume of oil produced by each well (row) in that year (columns)
-    yearly_volume <- matrix(0L, nrow=n_wet, ncol = n_years)
-    
-    # We know the initial production rates
-    bopd_year_beg[,1] <- final.ip
-    
-    # This could maybe be optimized using matrix math, but for loops will have to do for now
-    for (year in seq(1:n_years)) {
-      for (well_num in seq(1:n_wet)) {
-        if (year != 1) {
-          bopd_year_beg[well_num, year] = bopd_year_end[well_num, year-1]
-        }
-        bopd_year_end[well_num, year] = (1 - final.rod[well_num])*bopd_year_beg[well_num, year]
-        yearly_volume[well_num, year] = 365 * (bopd_year_beg[well_num, year] + bopd_year_end[well_num, year])/2
-      }
-    }
-  
-    # Calculate Net Present Revenue (NPR) -----------------------------------------------------------------
-    rev <- c()
-    operating_cost <- c()
-    
-    # Draw a net revenue interest (nri) from a normal distribution
-    nri <- rnorm(n = n_wet, mean = 0.75, sd = 0.02)
-    
-    for (year in 1:n_years) {
-      
-      # Draw the oil price from a random triangle distribution
-      oil_price <- rtriangle(n = n_wet, a = oil_pred$low_price[year],
-                             b = oil_pred$high_price[year],
-                             c = oil_pred$aeo_ref[year])
-      
-      #Grab that year's total volume produced
-      production <- yearly_volume[, year]
-      
-      # Calculate revenue for the year
-      rev <- cbind(rev,
-                   oil_price*production*nri*severance_tax)
-      
-      # Now calculate total operating costs
-      cost <- rnorm(n = 1, mean = 2.25, sd = 0.3)
-      total_operating_cost <- cost*production
-      operating_cost <- cbind(operating_cost, total_operating_cost)
-    }
-    # NPV ---------------------------------------------------------------------
-    sum <- 0
-    for (i in 1:n_years) {
-      sum <- sum + (rev[, i] - operating_cost[, i] - overhead_costs_wet) / (1.1^i)
-    }
-    
-    NPV <- -(completion_costs + seismic_costs[1:n_wet] + drilling_cost[1:n_wet] +
-               overhead_costs_wet + lease_costs[1:n_wet]) + sum
-  } else {
-    NPV <- 0
+  standardize <- function(x){
+    x.std = (x - mean(x))/sd(x)
+    return(x.std)
   }
+  
+  destandardize <- function(x.std, x){
+    x.old = (x.std * sd(x)) + mean(x)
+    return(x.old)
+  }
+  
+  # Create correlated values of initial production costs and rates of decline
+  # Underlying lognormal distribution should have mean ~ 6 and sd ~0.28
+  initial_production <- rlnorm(n = n_wet, meanlog = 6, sdlog = 0.28)
+  rate_of_decline <- runif(n = n_wet, min = 15, max = 32)
+  
+  # Standardize the vectors and "bend" them to correlate
+  Both.r <- cbind(standardize(rate_of_decline), standardize(initial_production))
+  SB.r <- U %*% t(Both.r)
+  SB.r <- t(SB.r)
+  
+  # Unstandardize the vectors
+  final.ip  <- destandardize(SB.r[,2], initial_production)
+  final.rod <- destandardize(SB.r[,1], rate_of_decline) / 100
+  
+  # Any negative rates corresond to a "dry" well
+  final.ip <- ifelse(final.ip < 0, 0, final.ip)
+  
+  # Check the correlation, if desired - my initial tests showed it was approx. 0.64
+  # cor(final.ip, final.rod)
+  
+  # Now calculate estimated production rates for 30 years using the calculated
+  # initial production and yearly decline rate
+  
+  # These matrices represent the production of oil in barrels per day
+  # Each row is one well
+  # Each column is the year (col 1 == year 1)
+  bopd_year_beg <- matrix(0L, nrow=n_wet, ncol = n_years)
+  bopd_year_end <- matrix(0L, nrow=n_wet, ncol = n_years)
+  
+  # This matrix represents the total volume of oil produced by each well (row) in that year (columns)
+  yearly_volume <- matrix(0L, nrow=n_wet, ncol = n_years)
+  
+  # We know the initial production rates
+  bopd_year_beg[,1] <- final.ip
+  
+  # This could maybe be optimized using matrix math, but for loops will have to do for now
+  for (year in seq(1:n_years)) {
+    for (well_num in seq(1:n_wet)) {
+      if (year != 1) {
+        bopd_year_beg[well_num, year] = bopd_year_end[well_num, year-1]
+      }
+      bopd_year_end[well_num, year] = (1 - final.rod[well_num])*bopd_year_beg[well_num, year]
+      yearly_volume[well_num, year] = 365 * (bopd_year_beg[well_num, year] + bopd_year_end[well_num, year])/2
+    }
+  }
+
+  # Calculate Net Present Revenue (NPR) -----------------------------------------------------------------
+  rev <- c()
+  operating_cost <- c()
+  
+  # Draw a net revenue interest (nri) from a normal distribution
+  nri <- rnorm(n = n_wet, mean = 0.75, sd = 0.02)
+  
+  for (year in 1:n_years) {
+    
+    # Draw the oil price from a random triangle distribution
+    oil_price <- rtriangle(n = n_wet, a = oil_pred$low_price[year + 1],
+                           b = oil_pred$high_price[year + 1],
+                           c = oil_pred$aeo_ref[year + 1])
+    
+    #Grab that year's total volume produced
+    production <- yearly_volume[, year]
+    
+    # Calculate revenue for the year
+    rev <- cbind(rev,
+                 oil_price*production*nri*severance_tax)
+    
+    # Now calculate total operating costs
+    cost <- rnorm(n = 1, mean = 2.25, sd = 0.3)
+    total_operating_cost <- cost*production
+    operating_cost <- cbind(operating_cost, total_operating_cost)
+  }
+  # NPV ---------------------------------------------------------------------
+  sum <- 0
+  for (i in 1:n_years) {
+    sum <- sum + (rev[, i] - operating_cost[, i] - overhead_costs_wet) / (1.1^i)
+  }
+  
+  NPV <- -(completion_costs + seismic_costs[1:n_wet] + drilling_cost[1:n_wet] +
+             overhead_costs_wet + lease_costs[1:n_wet]) + sum
   
   if (n_dry == 0) {
     dry_cost = 0
